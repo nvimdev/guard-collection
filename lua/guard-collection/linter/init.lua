7872d91ac1921ff37be4d2f1635239395e7b4646
Jon Surrell <sirreal@users.noreply.github.com>
2023-09-15 22:14:08 +0200

    Add eslint_d to linter init

--- lua/guard-collection/linter/init.lua
+++ lua/guard-collection/linter/init.lua
@@ -1,6 +1,7 @@
 return {
   ['clang-tidy'] = require('guard-collection.linter.clang-tidy'),
   codespell = require('guard-collection.linter.codespell'),
+  eslint_d = require('guard-collection.linter.eslint_d'),
   flake8 = require('guard-collection.linter.flake8'),
   hadolint = require('guard-collection.linter.hadolint'),
   luacheck = require('guard-collection.linter.luacheck'),
