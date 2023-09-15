641cbe70929b569628d53361afdfcca986310e23
Kassio Borges <kassioborgesm@gmail.com>
2023-09-16 00:26:28 +0200

    Fix rubocop

--- lua/guard-collection/linter/rubocop.lua
+++ lua/guard-collection/linter/rubocop.lua
@@ -4,19 +4,32 @@ return {
   cmd = 'bundle',
   args = { 'exec', 'rubocop', '--format', 'json', '--force-exclusion', '--stdin' },
   stdin = true,
-  parse = lint.from_json({
-    get_diagnostics = function(...)
-      return vim.json.decode(...).files[1].offenses
-    end,
-    attributes = {
-      lnum = 'location.line',
-      col = 'location.column',
-      code = 'cop_name',
-    },
-    severities = {
+  parse = function(result, bufnr)
+    local decoded = vim.json.decode(result)
+    local diagnostics = {}
+    local severities = {
       convention = lint.severities.info,
+      error = lint.severities.error,
+      fatal = lint.severities.fatal,
+      info = lint.severities.info,
       refactor = lint.severities.style,
-      fatal = lint.severities.error,
-    },
-  }),
+      warning = lint.severities.warning,
+    }
+
+    for _, diagnostic in ipairs(decoded.files[1].offenses) do
+      table.insert(diagnostics, {
+        bufnr = bufnr,
+        source = 'rubocop',
+        lnum = diagnostic.location.start_line,
+        end_lnum = diagnostic.location.last_line,
+        col = diagnostic.location.start_column,
+        end_col = diagnostic.location.last_column,
+        code = diagnostic.cop_name,
+        severity = severities[diagnostic.severity],
+        message = string.format('[%s] %s', diagnostic.cop_name, diagnostic.message),
+      })
+    end
+
+    return diagnostics
+  end,
 }
