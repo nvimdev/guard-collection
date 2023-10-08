bf2dce4975a6fb6874524590ff002ae380e21011
xiaoshihou514 <108414369+xiaoshihou514@users.noreply.github.com>
2023-10-08 12:23:43 +0100

    chore: stylua

--- test/formatter/sqlfluff_spec.lua
+++ test/formatter/sqlfluff_spec.lua
@@ -2,7 +2,7 @@ describe('sqlfluff', function()
   it('can format', function()
     local ft = require('guard.filetype')
     local tool = ft('sql'):fmt('sqlfluff')
-    tool.formatter[1].args = vim.list_extend(tool.formatter[1].args,{ '--dialect', 'ansi' })
+    tool.formatter[1].args = vim.list_extend(tool.formatter[1].args, { '--dialect', 'ansi' })
     require('guard').setup()
 
     local formatted = require('test.formatter.helper').test_with('sql', {
