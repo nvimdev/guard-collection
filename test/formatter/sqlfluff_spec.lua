a10de6d60721845ff65723169b0f7e349c345bd8
xiaoshihou <onelessononeexercise@gmail.com>
2023-11-01 09:36:51 +0000

    test: gofmt, golines, update README
    ktlint, golangci-lint, ormolu still broken
    test cov > 50% now

--- test/formatter/sqlfluff_spec.lua
+++ test/formatter/sqlfluff_spec.lua
@@ -21,4 +21,24 @@ describe('sqlfluff', function()
       [[    LIKE '%YOU%']],
     }, formatted)
   end)
+
+  it('can fix', function()
+    local ft = require('guard.filetype')
+    local tool = ft('sql'):fmt('sqlfluff_fix')
+    tool.formatter[1].args = vim.list_extend(tool.formatter[1].args, { '--dialect', 'ansi' })
+    require('guard').setup()
+
+    local formatted = require('test.formatter.helper').test_with('sql', {
+      [[SELECT]],
+      [[    a + b  AS foo,]],
+      [[    c AS bar]],
+      [[FROM my_table]],
+    })
+    assert.are.same({
+      [[SELECT]],
+      [[    c AS bar,]],
+      [[    a + b AS foo]],
+      [[FROM my_table]],
+    }, formatted)
+  end)
 end)
