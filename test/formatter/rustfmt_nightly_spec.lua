ca25be8a0fa7113f6c3d7b6ce81669dbe004d7d0
imbolc <me@imbolc.name>
2024-05-27 08:43:27 +0600

    Tests

--- test/formatter/rustfmt_nightly_spec.lua
+++ test/formatter/rustfmt_nightly_spec.lua
@@ -1,7 +1,7 @@
-describe('rustfmt', function()
+describe('rustfmt_nightly', function()
   it('can format', function()
     local ft = require('guard.filetype')
-    ft('rust'):fmt('rustfmt')
+    ft('rust'):fmt('rustfmt_nightly')
     require('guard').setup()
 
     local formatted = require('test.formatter.helper').test_with('rust', {
@@ -12,7 +12,7 @@ describe('rustfmt', function()
       [[}]],
     })
     assert.are.same({
-      [[use std::{collections::HashMap, collections::HashSet};]],
+      [[use std::collections::{HashMap, HashSet};]],
       [[fn main() {]],
       [[    let var: usize = 1;]],
       [[    println!("{var}");]],
